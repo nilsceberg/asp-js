15e58605bd3d9a665aacbf2d8e7269b9bb784522
Nils Ceberg <nils.ceberg@gmail.com>
2019-06-17 02:21:49 +0200

    Print parsing results without custom inspectors

--- parse.sh
+++ parse.sh
@@ -2,5 +2,5 @@
 
 file=$1
 
-node -e 'Script = require("./build/runtime/Script").Script; console.log(util.inspect(Script.fromFile("'$file'"), { depth: null, colors: true }))'
+node -e 'Script = require("./build/runtime/Script").Script; console.log(util.inspect(Script.fromFile("'$file'"), { depth: null, colors: true, customInspect: false }))'
 
